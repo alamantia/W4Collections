515bb805ea7425880dc062395f1cf35079086edf
waffles <waffles@waffless-MacBook-Pro.local>
2014-03-12 17:58:26 -0500

    inital commit

---
+++ W4Collections/W4Collections/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  W4Collections
+//
+//  Created by waffles on 3/12/14.
+//  Copyright (c) 2014 w4ffles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ W4Collections/W4CollectionsTests/W4CollectionsTests.m
@@ -0,0 +1,34 @@
+//
+//  W4CollectionsTests.m
+//  W4CollectionsTests
+//
+//  Created by waffles on 3/12/14.
+//  Copyright (c) 2014 w4ffles. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface W4CollectionsTests : XCTestCase
+
+@end
+
+@implementation W4CollectionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
